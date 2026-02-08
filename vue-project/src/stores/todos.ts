44581b565684be6583fa2500b5174c48d4b0325d
Mingiyan <nemkeev.m@sunktru>
2026-02-08 22:09:46 +0300

    feat: add store actions

---
+++ vue-project/src/stores/todos.ts
@@ -0,0 +1,45 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+interface IToDo {
+  id: number
+  name: string
+  description: string
+  completed: boolean
+}
+
+type CreateToDo = Pick<IToDo, 'name' | 'description'> 
+type UpdateToDo = Partial<Pick<IToDo, 'name' | 'description' | 'completed'>>
+
+export const useTodosStore = defineStore('counter', () => {
+  const todos = ref<IToDo[]>([])
+  const nextId = ref(1)
+
+  const add = (todo: CreateToDo) => {
+    todos.value.push({
+      id: nextId.value++,
+      name: todo.name,
+      description: todo.description,
+      completed: false
+    })
+  }
+
+  const update = (id: number, todo: UpdateToDo) => {
+    const index = todos.value.findIndex((i) => i.id === id )
+    if (index == -1 || !todos.value[index]) return false
+    todos.value[index] = {
+      ...todos.value[index],
+      ...todo
+    }
+    return true
+  }
+
+  const remove = (id: number) => {
+    const index = todos.value.findIndex((i) => i.id == id)
+    if (index == -1 || !todos.value[index]) false
+    todos.value.splice(index, 1)
+  }
+
+
+  return { todos, add, update, remove }
+})
